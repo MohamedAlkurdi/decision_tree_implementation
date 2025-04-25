03cb7cb33b72b8931ec1478a3a07c15ce776ce6c
MohamedAlkurdi <muhammed.alkurdi.he@gmail.com>
2025-04-25 18:05:05 +0300

    documentation

--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,6 @@
 import math
 import pandas as pd
-
-def execlude_root_feature(data,best_feature):
-    features = list(data.columns)
-    features.remove(best_feature)
-    return features
-
-
-def detect_feature_data_types(column):
-    if isinstance(column, pd.Series):
-        column = column.tolist()
-    
-    classes = list(set(column))
-    classes_split = {}
-    proportions = []
-
-    for c in classes:
-        classes_split[c] = column.count(c)
-    for i in classes_split:
-        proportions.append(classes_split[i] / len(column))
-
-    return proportions, classes_split
+from .datalab import *
 
 
 def entropy(column):
@@ -37,16 +17,6 @@ def entropy(column):
             entropy += abs(p * math.log2(p))
 
     return entropy
-
-
-def equivalent_target_values(feature_classes, feature_column, target_column):
-    classes_dict = {}
-    for j in range(len(feature_classes)):
-        classes_dict[feature_classes[j]] = []
-    for i in range(len(feature_column)):
-        classes_dict[feature_column[i]].append(target_column[i])
-    return classes_dict
-
 
 def information_gain(target_column, feature_column):
     if isinstance(target_column, pd.Series):
